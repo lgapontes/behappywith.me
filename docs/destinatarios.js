f24247f672f6b0ff95a4b26c8f74f7679abfd692
lgapontes <lgapontes@gmail.com>
2017-06-16 21:59:21 -0300

    cap1

---
+++ docs/destinatarios.js
@@ -0,0 +1,9 @@
+let destinatarios = [
+   { "descricao": "Pais" },
+   { "descricao": "Irmãos" },
+   { "descricao": "Filhos" },
+   { "descricao": "Parceiros" },
+   { "descricao": "Amigos" },
+   { "descricao": "Colegas" },
+   { "descricao": "Desconhecidos" }
+];
---
+++ docs/usuario.js
@@ -0,0 +1,14 @@
+let usuario = {
+   "email": "usuario@site.com.br",
+   "senha": "secreta",
+   "nome": "Fulano",
+   "xp": 7,
+   "cor": "#4a148c",
+   "gentilezas": [{
+       "tipo": { "descricao": "Zap", "xp": 5 },
+       "destinatario": { "descricao": "Filhos" },
+       "descricao": "Zap para filhos!",
+       "likes": 2,
+       "timestamp": "2017-06-10T22:05:22-03:00"
+   }]
+};
