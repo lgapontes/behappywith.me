0d735f90bf9e98fb6851cd61708a84ba64fee4a5
lgapontes <lgapontes@gmail.com>
2017-07-01 01:55:36 -0300

    cap3

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './img/favicon.ico';
+import './css/index.css';
+import './css/pure-min.css';
 
 ReactDOM.render(
     <h1>Bem-vindo ao React!</h1>,
--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src/index.jsx'),
@@ -12,7 +14,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.join(__dirname, 'src/index.html')
-        })
+        }),
+        new ExtractTextPlugin('style.css'),
+        new UglifyJSPlugin()
     ],
     module: {
         rules: [
@@ -32,6 +36,13 @@ module.exports = {
         {
             test: /\.(jpe?g|ico|png|gif|svg)$/i,
             loader: 'file-loader?name=img/[name].[ext]'
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
         }
         ]
     },
