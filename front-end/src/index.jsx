18a1442faaa1a1e55d05f51f0f41a80f9d7da07a
lgapontes <lgapontes@gmail.com>
2018-01-30 01:23:39 -0200

    manifest.json

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './img/favicon.ico';
+import './img/icon-72x72.png';
+import './img/icon-96x96.png';
+import './img/icon-128x128.png';
+import './img/icon-144x144.png';
+import './img/icon-152x152.png';
+import './img/icon-192x192.png';
+import './img/icon-384x384.png';
+import './img/icon-512x512.png';
 
 import './css/index.css';
 import './css/pure-min.css';
